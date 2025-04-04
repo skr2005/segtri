94cf71b2a58319218a59944084d77a1c49f3327e
skr2005 <...>
2025-04-04 18:58:04 +0800

    chore: cargo fmt

--- src/modify_op.rs
+++ src/modify_op.rs
@@ -3,13 +3,13 @@
 /// such as [crate::SegTree::modify].
 pub trait ModifyOp<T>: PartialEq + Clone {
     /// The modification function.
-    /// 
-    /// The given `orig_seg_data` is the reference to the sum of data 
+    ///
+    /// The given `orig_seg_data` is the reference to the sum of data
     /// in a segment of length `seg_len`.
-    /// 
-    /// This function should modify `orig_seg_data`, 
+    ///
+    /// This function should modify `orig_seg_data`,
     /// behaving like applying the operation multiple times (`n` times)
-    /// to data of every point of the segment. 
+    /// to data of every point of the segment.
     /// It should does nothing when patameter `n` is zero.
     fn modify_range_ntimes(
         &self,
